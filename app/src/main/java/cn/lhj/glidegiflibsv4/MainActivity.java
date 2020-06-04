875acd2f6ed435293268277867da255d9d8e59b5
xingxing <xxx823952375@gmail.com>
2020-06-04 11:49:43 +0800

    更新gradle

--- app/src/main/java/cn/lhj/glidegiflibsv4/MainActivity.java
+++ app/src/main/java/cn/lhj/glidegiflibsv4/MainActivity.java
@@ -1,8 +1,8 @@
 package cn.lhj.glidegiflibsv4;
 
+import android.os.Bundle;
 import android.support.rastermill.FrameSequenceDrawable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ImageView;
 
 import cn.lhj.glidegif.util.ImageGifLoader;
@@ -28,15 +28,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadImage() {
-        String url = "http://hiphotos.baidu.com/feed/pic/item/71cf3bc79f3df8dc41ae9d2dc611728b461028fe.jpg";
+//        String url = "http://hiphotos.baidu.com/feed/pic/item/71cf3bc79f3df8dc41ae9d2dc611728b461028fe.jpg";
         String url1 = "https://img.zcool.cn/community/01644a5a177ed0a8012051873fd06a.gif";
         String url2 = "http://n.sinaimg.cn/games/crawl/20160921/Pol--fxvyqwa3680622.gif";
         String url3 = "http://n.sinaimg.cn/games/crawl/20160921/j3Vi-fxvyzus2219268.gif";
         String url4 = "http://n.sinaimg.cn/games/crawl/20160921/-9ps-fxvyqwe0343025.gif";
-        ImageGifLoader.display(this,url,iv, FrameSequenceDrawable.LOOP_INF);
+//        ImageGifLoader.display(this,url,iv, FrameSequenceDrawable.LOOP_INF);
         ImageGifLoader.display(this,url1,iv1, FrameSequenceDrawable.LOOP_INF);
-        ImageGifLoader.display(this,url2,iv2, FrameSequenceDrawable.LOOP_INF);
-        ImageGifLoader.display(this,url3,iv3, FrameSequenceDrawable.LOOP_INF);
-        ImageGifLoader.display(this,url4,iv4, FrameSequenceDrawable.LOOP_INF);
+        ImageGifLoader.display(this,url1,iv2, FrameSequenceDrawable.LOOP_INF);
+        ImageGifLoader.display(this,url1,iv3, FrameSequenceDrawable.LOOP_INF);
+        ImageGifLoader.display(this,url1,iv4, FrameSequenceDrawable.LOOP_INF);
     }
 }
