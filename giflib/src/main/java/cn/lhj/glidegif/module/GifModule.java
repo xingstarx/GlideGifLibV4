734a18b5b88044b4665ae9666c995440c5bcd0cf
xingxing <xxx823952375@gmail.com>
2020-06-04 13:09:22 +0800

    LibraryGlideModule

--- giflib/src/main/java/cn/lhj/glidegif/module/GifModule.java
+++ giflib/src/main/java/cn/lhj/glidegif/module/GifModule.java
@@ -8,7 +8,6 @@ import android.support.rastermill.FrameSequenceDrawable;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.module.LibraryGlideModule;
 
 import java.io.InputStream;
@@ -23,17 +22,13 @@ import cn.lhj.glidegif.GifDrawableTranscoder;
  * @date 2018/10/29
  */
 @GlideModule
-public class GifModule extends AppGlideModule{
+public class GifModule extends LibraryGlideModule {
 
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
         super.registerComponents(context, glide, registry);
-        registry.register(FrameSequence.class,FrameSequenceDrawable.class,new GifDrawableTranscoder()).append(InputStream.class,FrameSequence.class,new GifDecoder());
+        registry.register(FrameSequence.class, FrameSequenceDrawable.class, new GifDrawableTranscoder()).append(InputStream.class, FrameSequence.class, new GifDecoder());
 
-    }
-
-    @Override
-    public boolean isManifestParsingEnabled() {
-        return false;
     }
 }
+
